5dfb6086dc3e960ba649865f8c0da3fad27622f8
Refired <alexshepherdno1@gmail.com>
2025-12-03 23:13:48 +0000

    Initial Commit

---
+++ frontEnd/src/App.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+</template>
+
+<style>
+  body,html {
+    margin:0;
+    padding:0;
+  }
+</style>
---
+++ frontEnd/src/views/index.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+
+</script>
+
+<template>
+    <div class="headerIndex">
+
+    </div>
+    <div class="mainIndex">
+        
+    </div>
+</template>
+
+<style scoped>
+
+</style>
